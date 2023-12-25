31cb2b56a5783c8255c13002e8fd6d0c58e1225e
Pedro Lopes <pl.pedrogplopes@gmail.com>
2023-12-25 18:02:27 -0300

    Update calendariodofla.py

--- calendariodofla.py
+++ calendariodofla.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from oponente import oponente
 from info_jogo import competicao, dia, hora
 from config import api_key, api_secret, bearer_token, access_token, access_token_secret
+# Você pode reproduzir a execução em outra conta criando um arquivo config.py e inserindo suas chaves do Twitter Developer Portal
 
 client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_token_secret)
 auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
