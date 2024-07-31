551a4116d8653238800f7cef6f8edb34ba236623
Pedro Lopes <pl.pedrogplopes@gmail.com>
2024-07-31 11:36:46 -0300

    Update oponente.py

--- oponente.py
+++ oponente.py
@@ -1,5 +1,6 @@
 import re
 import requests
+import json
 
 # URL da agenda de jogos do Flamengo do Globo Esporte
 url = "https://ge.globo.com/futebol/times/flamengo/agenda-de-jogos-do-flamengo/#/"
@@ -8,19 +9,39 @@ url = "https://ge.globo.com/futebol/times/flamengo/agenda-de-jogos-do-flamengo/#
 response = requests.get(url)
 html_content = response.text
 
-# Usando expressões regulares para encontrar a primeira ocorrência entre "match" e "location"
-proximo_jogo_oponente = re.search(r'match.*?location"', html_content, re.DOTALL | re.UNICODE)
+# Usando expressões regulares para encontrar o JSON dentro de window.byTeamScheduleTeamData
+json_data_match = re.search(r'window\.byTeamScheduleTeamData\s*=\s*({.*?});', html_content, re.DOTALL)
 
-# Se encontrou a correspondência, extraia as instâncias após "popularName" entre aspas
-if proximo_jogo_oponente:
-    proximo_jogo_texto = proximo_jogo_oponente.group(0)
-    
-    # Usando expressão regular para encontrar todas as instâncias após "popularName" entre aspas
-    times = re.findall(r'popularName":"(.*?)"', proximo_jogo_texto)
+# Inicializando a variável do oponente
+oponente = None
 
-    # Verificando a instância diferente de "Flamengo" e armazenando em "oponente"
-    oponente = next((instancia for instancia in times if instancia != "Flamengo"), None)
+# Se encontrou o JSON
+if json_data_match:
+    json_data = json_data_match.group(1)
 
-    # Se encontrou a correspondência, imprima o resultado
-    if oponente:
-        print(f"Oponente: {oponente}")
+    try:
+        # Carregar a string JSON como um dicionário
+        data = json.loads(json_data)
+
+        # Extrair informações relevantes
+        match_info = data.get('matches', [])[0]  # Primeiro jogo listado
+        if match_info:
+            # Identificar os times participantes
+            time1 = match_info.get('firstContestant', {}).get('popularName', None)
+            time2 = match_info.get('secondContestant', {}).get('popularName', None)
+
+            # Identificar o oponente
+            if time1 and time1 != "Flamengo":
+                oponente = time1
+            elif time2 and time2 != "Flamengo":
+                oponente = time2
+
+            if oponente:
+                print(f"Oponente: {oponente}")
+            else:
+                print("Oponente não encontrado.")
+
+    except json.JSONDecodeError:
+        print("Erro ao decodificar JSON.")
+else:
+    print("Informações do próximo jogo não encontradas.")
